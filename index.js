8b03ed030eebea6852cf075607be76b803d18222
yzk-0283 <131577199+yzk-0283@users.noreply.github.com>
2023-10-24 21:36:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require("koa");
 const Router = require("koa-router");
 const logger = require("koa-logger");
+const request = require('request');
 const bodyParser = require("koa-bodyparser");
 const fs = require("fs");
 const path = require("path");
@@ -14,6 +15,44 @@ const homePage = fs.readFileSync(path.join(__dirname, "index.html"), "utf-8");
 router.get("/", async (ctx) => {
   ctx.body = homePage;
 });
+
+
+// 订阅消息
+app.get("/send", async function (req, res) {
+  const { openid } = req.query // 通过 get 参数形式指定 openid
+  // 在这里直接是触发性发送，也可以自己跟业务做绑定，改成事件性发送
+  const info = await sendapi(openid)
+  res.send(info)
+});
+
+async function sendapi(openid) {
+  return new Promise((resolve, reject) => {
+    request({
+      url: "http://api.weixin.qq.com/cgi-bin/message/subscribe/send",
+      method: "POST",
+      body: JSON.stringify({
+        touser: openid,
+        template_id: "这 T28aWToEBXR29Ti6MVs-ujC2kgXTuapAS0Fhn0XQa_Y",
+        miniprogram_state: "developer",
+        data: {
+          // 这里替换成自己的模板 ID 的详细事项，不要擅自添加或更改
+          // 按照 key 前面的类型，对照参数限制填写，否则都会发送不成功
+          //
+          thing4: {
+            value: "这是一个提醒",
+          },
+          time5: {
+            value: "2022 年 4 月 26 日 21:48",
+          },
+        },
+      }),
+    },function(error,res){
+      if(error) reject(error)
+      resolve(res.body)
+    });
+  });
+}
+
 
 // 更新计数
 router.post("/api/count", async (ctx) => {
